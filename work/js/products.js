91cc63fa2382d0dab7b47fe7bbb587e81060969a
ilearnbydoing <dgweb123@gmail.com>
2018-09-14 11:36:08 +0530

    first commit

---
+++ work/js/products.js
@@ -0,0 +1,115 @@
+var $products,
+
+	$current_product = 'smtpconnect';
+
+
+
+// List all the products here
+
+
+
+$products = {
+		smtpconnect : {
+
+		name     : 'Smtpconnect.com',
+
+		tag      : 'PHP',
+
+		img      : 'images/smtpconnect.jpg',
+
+		url      : 'https://smtpconnect.com',
+
+		purchase : '#',
+
+	},
+
+		newrampgreen : {
+
+		name     : 'New Ram Green Technologies',
+
+		tag      : 'WP',
+
+		img      : 'images/newrampgreen.jpg',
+
+		url      : 'http://newrampgreen.com/',
+
+		purchase : '#',
+
+	},
+
+		niibindia : {
+
+		name     : 'Niib India',
+
+		tag      : 'WP',
+
+		img      : 'images/niibindia.jpg',
+
+		url      : 'http://niibindia.org/',
+
+		purchase : '#',
+
+	},
+		sanskara : {
+
+		name     : 'Sanskara Trust',
+
+		tag      : 'WP',
+
+		img      : 'images/sanskara.jpg',
+
+		url      : 'http://sanskara.org',
+
+		purchase : '#',
+
+	},
+		w3update : {
+
+		name     : 'W3update',
+
+		tag      : 'WP',
+
+		img      : 'images/w3update.jpg',
+
+		url      : 'https://www.w3update.com/',
+
+		purchase : '#',
+
+	},
+
+	welgrowindia : {
+
+		name     : 'Welgrowindia.com',
+
+		tag      : 'WP',
+
+		img      : 'images/welgrowindia.jpg',
+
+		url      : 'http://welgrowindia.com/',
+
+		purchase : '#',
+
+	},
+
+	ti2 : {
+
+		name     : 'Ti2.com.au',
+
+		tag      : 'WP',
+
+		img      : 'images/ti2.jpg',
+
+		url      : 'https://ti2.com.au/',
+
+		purchase : '#',
+
+	},
+
+
+
+	
+
+
+
+};
+
